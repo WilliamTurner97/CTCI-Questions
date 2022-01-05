916f4ca5280a8c9364cffa9eeb63f750ac7f70b1
WilliamTurner97 <wct15@case.edu>
2022-01-05 17:56:45 -0500

    CheckPermutation fixed for ASCII

--- CheckPermutation.py
+++ CheckPermutation.py
@@ -1,13 +1,13 @@
 # 1.2 Check Permutation
 
 def check_permutation(l1, l2):
-    count1 = [0] * 26
-    count2 = [0] * 26
+    count1 = [0] * 128
+    count2 = [0] * 128
 
     for cr in l1:
-        count1[ord(cr) - 97] += 1
+        count1[ord(cr)] += 1
     for cr in l2:
-        count2[ord(cr) - 97] += 1
+        count2[ord(cr)] += 1
 
     return count1 == count2
 
