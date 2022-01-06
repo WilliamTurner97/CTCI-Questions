0dad4fea84684920088f1b4e124fa78514d97b7a
WilliamTurner97 <wct15@case.edu>
2022-01-05 19:36:43 -0500

    URLify initial

--- URLify.py
+++ URLify.py
@@ -1,2 +1,24 @@
 # 1.3 URLify
 
+def urlify(sl):
+
+    i = len(sl) - 1
+    j = len(sl) - 2
+
+    while j >= 0:
+        if sl[j] == None:
+            j -= 1
+            continue
+        if sl[j] != ' ':
+            sl [i] = sl[j]
+            i -= 1
+            j -= 1
+        else:
+            sl[i-2:i+1] = ['%', '2', '0']
+            i -= 3
+            j -= 1
+
+    return sl
+
+
+print(urlify(['a', 'b', ' ', 'c', 'd', 'e', ' ', 'e', None, None, None, None]))
