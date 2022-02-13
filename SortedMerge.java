0654c54d9d0816af4b4930dee217660f88902943
WilliamTurner97 <wct15@case.edu>
2022-02-12 21:46:44 -0500

    -SortedMerge fix

--- SortedMerge.java
+++ SortedMerge.java
@@ -3,7 +3,7 @@ import java.lang.Integer;
 public class SortedMerge {
 
     public static void main(String[] args) {
-        Integer[] test = sortAB(new Integer[] {Integer.valueOf(1), null}, new Integer[] {Integer.valueOf(2)});
+        Integer[] test = sortAB(new Integer[] {1, null}, new Integer[] {2});
         for(Integer x : test) {
             System.out.println(x.toString());
         }
@@ -18,8 +18,8 @@ public class SortedMerge {
             i--;
         }
 
-        while(i != -1 && j != -1) {
-            if(i == -1 || B[j].intValue() > A[i].intValue()) {
+        while(j != -1) {
+            if(B[j] > A[i]) {
                 A[k] = B[j];
                 j--;
             }
